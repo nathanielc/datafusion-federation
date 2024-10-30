cedd322ffb8177d679045a600c2317b2eaec502e
Nathaniel Cook <nvcook42@gmail.com>
2024-10-30 15:19:48 -0600

    wip: adds support for dynamic tables within a catalog

--- datafusion-flight-sql-table-provider/src/lib.rs
+++ datafusion-flight-sql-table-provider/src/lib.rs
@@ -1,9 +1,13 @@
 use std::sync::Arc;
 
-use arrow::{datatypes::SchemaRef, error::ArrowError};
-use arrow_flight::sql::client::FlightSqlServiceClient;
+use arrow::{array::RecordBatch, compute::concat_batches, datatypes::SchemaRef, error::ArrowError};
+use arrow_flight::{
+    sql::{client::FlightSqlServiceClient, CommandGetTables},
+    FlightInfo,
+};
 use async_trait::async_trait;
 use datafusion::{
+    common::cast::as_string_array,
     error::{DataFusionError, Result},
     physical_plan::{stream::RecordBatchStreamAdapter, SendableRecordBatchStream},
     sql::unparser::dialect::{DefaultDialect, Dialect},
@@ -14,13 +18,15 @@ use tonic::transport::Channel;
 
 pub struct FlightSQLExecutor {
     context: String,
+    catalog: String,
     client: FlightSqlServiceClient<Channel>,
 }
 
 impl FlightSQLExecutor {
-    pub fn new(dsn: String, client: FlightSqlServiceClient<Channel>) -> Self {
+    pub fn new(dsn: String, catalog: String, client: FlightSqlServiceClient<Channel>) -> Self {
         Self {
             context: dsn,
+            catalog,
             client,
         }
     }
@@ -29,17 +35,11 @@ impl FlightSQLExecutor {
         self.context = context;
     }
 }
-
-async fn make_flight_sql_stream(
-    sql: String,
+async fn make_flight_stream(
+    flight_info: FlightInfo,
     mut client: FlightSqlServiceClient<Channel>,
     schema: SchemaRef,
 ) -> Result<SendableRecordBatchStream> {
-    let flight_info = client
-        .execute(sql.to_string(), None)
-        .await
-        .map_err(arrow_error_to_df)?;
-
     let mut flight_data_streams = Vec::with_capacity(flight_info.endpoint.len());
     for endpoint in flight_info.endpoint {
         let ticket = endpoint.ticket.ok_or(DataFusionError::Execution(
@@ -56,6 +56,19 @@ async fn make_flight_sql_stream(
         schema,
         record_batch_stream,
     )))
+}
+
+async fn make_flight_sql_stream(
+    sql: String,
+    mut client: FlightSqlServiceClient<Channel>,
+    schema: SchemaRef,
+) -> Result<SendableRecordBatchStream> {
+    let flight_info = client
+        .execute(sql.to_string(), None)
+        .await
+        .map_err(arrow_error_to_df)?;
+
+    make_flight_stream(flight_info, client, schema).await
 }
 
 #[async_trait]
@@ -78,9 +91,34 @@ impl SQLExecutor for FlightSQLExecutor {
     }
 
     async fn table_names(&self) -> Result<Vec<String>> {
-        Err(DataFusionError::NotImplemented(
-            "flight_sql source: table inference not implemented".to_string(),
-        ))
+        let flight_info = self
+            .client
+            .clone()
+            .get_tables(CommandGetTables {
+                catalog: Some(self.catalog.to_string()),
+                db_schema_filter_pattern: None,
+                table_name_filter_pattern: None,
+                table_types: vec![],
+                include_schema: false,
+            })
+            .await?;
+        let schema = Arc::new(
+            flight_info
+                .clone()
+                .try_decode_schema()
+                .map_err(arrow_error_to_df)?,
+        );
+        let stream = make_flight_stream(flight_info, self.client.clone(), schema.clone()).await?;
+        let batches: Vec<RecordBatch> = stream.try_collect().await?;
+        let batch = concat_batches(&schema, batches.iter())?;
+        let names = batch.column_by_name("table_name").ok_or_else(|| {
+            DataFusionError::Internal("table_name column should exist".to_string())
+        })?;
+        Ok(as_string_array(names)?
+            .into_iter()
+            .flatten()
+            .map(|name| name.to_owned())
+            .collect())
     }
 
     async fn get_table_schema(&self, table_name: &str) -> Result<SchemaRef> {
